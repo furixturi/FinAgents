734947079e9726ac82b58eaa817da868cf28c65f
Xiaoli Alex Shen <alabebop@gmail.com>
2024-06-13 23:21:47 +0900

    fix typo bugs in post crud file

--- backend/app/crud/post.py
+++ backend/app/crud/post.py
@@ -11,7 +11,7 @@ async def create_post(session: AsyncSession, post_create: PostCreate):
     user = result.scalar_one_or_none()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    
+
     post = Post(
         title=post_create.title,
         content=post_create.content,
@@ -33,11 +33,11 @@ async def get_posts(session: AsyncSession):
 async def update_post(session: AsyncSession, post_id: int, post_update: PostUpdate):
     try:
         result = await session.execute(
-            select(Post).filter_by(id=post_id)
-        ).with_for_update()
+            select(Post).filter_by(id=post_id).with_for_update()
+        )
         post = result.scalar_one_or_none()
         if post is None:
-            raise HTTPException(status_code=404, defail="Post not found")
+            raise HTTPException(status_code=404, detail="Post not found")
         post.title = post_update.title
         post.content = post_update.content
         post.file_url = post_update.file_url
