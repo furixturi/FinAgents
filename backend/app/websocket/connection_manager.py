2a2eea311297c7f5263c812393808380af1fa3b7
Xiaoli Alex Shen <alabebop@gmail.com>
2024-06-18 11:24:12 +0900

    change websocket connection manager connection id from user_id to more general term connection_id

--- backend/app/websocket/connection_manager.py
+++ backend/app/websocket/connection_manager.py
@@ -5,17 +5,17 @@ class ConnectionManager:
     def __init__(self) -> None:
         self.active_connections: Dict[int, List[WebSocket]] = {}
         
-    async def connect(self, user_id: int, websocket: WebSocket):
+    async def connect(self, connection_id: int, websocket: WebSocket):
         await websocket.accept()
-        if user_id in self.active_connections:
-            self.active_connections[user_id].append(websocket)
+        if connection_id in self.active_connections:
+            self.active_connections[connection_id].append(websocket)
         else:
-            self.active_connections[user_id] = [websocket]
+            self.active_connections[connection_id] = [websocket]
             
-    def disconnect(self, user_id: int, websocket: WebSocket):
-        self.active_connections[user_id].remove(websocket)
-        if not self.active_connections[user_id]:
-            del self.active_connections[user_id]
+    def disconnect(self, connection_id: int, websocket: WebSocket):
+        self.active_connections[connection_id].remove(websocket)
+        if not self.active_connections[connection_id]:
+            del self.active_connections[connection_id]
             
     async def send_personal_message(self, message: Dict, websocket: WebSocket):
         await websocket.send_json(message)
